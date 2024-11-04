3ff759abd4f0794f668405dc22887748dec432d6
ozymandias20 <yamiligomez20@gmail.com>
2024-11-04 20:45:51 -0300

    feat: Implementación de login y registro de usuarios
    - Añadida pantalla de inicio de sesión (LoginScreen.tsx) con validación local de usuario.
    - Añadida pantalla de registro de usuarios (RegisterScreen.tsx) con almacenamiento de token en AsyncStorage.
    - Actualizada la navegación (Navigation.tsx) para incluir las pantallas de login y registro.
    - Integrado AsyncStorage para manejo de tokens de autenticación.
    - Añadidas las siguientes librerías:
      - @react-native-async-storage/async-storage para el almacenamiento local de tokens.
    - Añadido botón de logout en HomeScreen.tsx para cerrar sesión y redirigir al login.

--- src/navigator/Navigation.tsx
+++ src/navigator/Navigation.tsx
@@ -1,28 +1,59 @@
-import React from 'react';1
-
+import React, { useEffect, useState } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import { DetailScreen } from '../screens/DetailScreen';
 import { HomeScreen } from '../screens/HomeScreen';
+import { LoginScreen } from '../screens/LoginScreen';
 import { Movie } from '../interfaces/movieInterface';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { NavigationContainer } from '@react-navigation/native';
+import { ActivityIndicator, View } from 'react-native';
 
 export type RootStackParams = {
   HomeScreen: undefined;
   DetailScreen: Movie;
-
-}
+  LoginScreen: undefined;
+};
 
 const Stack = createStackNavigator<RootStackParams>();
 
 export const Navigation = () => {
-  return (
-    <Stack.Navigator
-        screenOptions={{
-            headerShown: false,
+  const [isLoading, setIsLoading] = useState(true);
+  const [userToken, setUserToken] = useState<string | null>(null);
 
+  useEffect(() => {
+    const checkToken = async () => {
+      const token = await AsyncStorage.getItem('userToken');
+      setUserToken(token);
+      setIsLoading(false);
+    };
+
+    checkToken();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ActivityIndicator size="large" color="#0000ff" />
+      </View>
+    );
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
         }}
-    >
-      <Stack.Screen name="HomeScreen" component={HomeScreen} />
-      <Stack.Screen name="DetailScreen" component={DetailScreen} />
-    </Stack.Navigator>
+      >
+        {userToken ? (
+          <>
+            <Stack.Screen name="HomeScreen" component={HomeScreen} />
+            <Stack.Screen name="DetailScreen" component={DetailScreen} />
+          </>
+        ) : (
+          <Stack.Screen name="LoginScreen" component={LoginScreen} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
